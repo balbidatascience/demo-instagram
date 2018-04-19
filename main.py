d9e14ca3beda5a4fd31d10798afec51096bdb0ef
Gabriel Machado <gabrielopes@gmail.com>
2018-04-19 15:57:59 -0300

    list photos ranking by likes

--- main.py
+++ main.py
@@ -1,35 +1,73 @@
+import sys
 import requests
+import pandas as pd
 
 BASE_URL = 'https://api.instagram.com/v1/'
 ACCESS_TOKEN = '315156082.25f5d28.2633198fd77246e28e73855d7a020a9a'
 
+
 def getOwnPost():
-    request_url = 'https://api.instagram.com/v1/users/self/?access_token=%s' %ACCESS_TOKEN
-    print('Request: %s' %request_url)
+    request_url = 'https://api.instagram.com/v1/users/self/?access_token=%s' % ACCESS_TOKEN
+    print('Request: %s' % request_url)
 
     request_post = requests.get(request_url).json()
     print(request_post)
 
-def getRecentMedia():
-    request_url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=%s' %ACCESS_TOKEN
 
+def getRecentMedia(ACCESS_TOKEN='315156082.25f5d28.2633198fd77246e28e73855d7a020a9a'):
+    request_url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=%s' % ACCESS_TOKEN
     request_post = requests.get(request_url).json()
-    #print(request_post)
+    print(request_url)
+
+    photoID = []
+    countLikes = []
+    createTime = []
+    images = []
+    df = {'photoId': photoID, 'countLikes': countLikes, 'createdTime': createTime, 'images': images}
 
     if request_post['meta']['code'] == 200:
 
-        if(len(request_post['data']) > 0):
-             print(request_post['data'][0]['id'])
-             print(request_post['data'][0]['user']['full_name'])
-             print(request_post['data'][0]['user']['username'])
+        if len(request_post['data']) > 0:
+            for obj in request_post['data']:
+
+                photoID.append(obj['id'])
+                countLikes.append(obj['likes']['count'])
+                createTime.append(obj['created_time'])
+                images.append(obj['images']['standard_resolution']['url'])
+
+                #print('Foto id: %s' % obj['id'])
+                #print('Foto: %s' % obj['images']['standard_resolution']['url'])
+                #print('Qtde de likes: %s' % obj['likes']['count'])
+                #print('Data do post: %s' % obj['created_time'])
+
         else:
             print('No post to show')
 
+        df = pd.DataFrame(df)
+        #print(df.sort_values('countLikes', ascending=False).head(10))
+        #print(df.describe())
         #print(request_post)
+
     else:
-        print('Error ')
+        print('Error')
+
+    return df
 
 
+if __name__ == '__main__':
 
-#getOwnPost()
-getRecentMedia()
+    # Versão para teste - gabrielmlg account
+    df = getRecentMedia()
+
+    # Versão de produção.
+    #df = getRecentMedia(sys.argv[1])
+
+    # Raking de fotos por likes:
+    print(df.sort_values('countLikes', ascending=False).head(10))
+    count = 0
+
+    for x in df['images']:
+        count = count + 1
+        ran = count
+        print('%s - %s' % (str(ran), x))
+
